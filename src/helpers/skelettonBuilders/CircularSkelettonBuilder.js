1c9c013c35ed1f4c6c38f47523a88cc478e73566
KerDelos <damien.delmas@etu.utc.fr>
2017-01-16 06:19:31 +0100

    weighted barycentre for circular graph

--- src/helpers/skelettonBuilders/CircularSkelettonBuilder.js
+++ src/helpers/skelettonBuilders/CircularSkelettonBuilder.js
@@ -64,66 +64,42 @@ class CircularSkelettonBuilder{
         _.forEach(parsedData.contacts,( messages, contactID ) => {
             let contactNode = new Node(contactID, {color: "#d54209", labelColor: "node", size: Math.sqrt((_.sumBy(messages, "weight")) / maxContactWeight) * 33 + 2});
             let maxWeight = _.maxBy(messages, "weight").weight;
-            let relatedNodes = [];
+
+            let xSum = 0;
+            let ySum = 0;
+            let total = 0;
             _.forEach(messages, (msg) => {
 
                 let msgTimestamp = moment(msg.timestamp).year(1970).valueOf();
                 let index = interpolationSearch(skelTimestamps, msgTimestamp);
                 let target = new Node(skelTimestamps[index]);
 
-                relatedNodes.push(skelNodes[index]);
+                let n = skelNodes[index];
+                if( n ){
+                    xSum += n.x*msg.weight;
+                    ySum += n.y*msg.weight;
+                    total += msg.weight;
+                }
+
 
                 res.addEdge(new Edge(contactNode, target, {color: "rgba(0,0,0,0.05)", weight: Math.pow(msg.weight / maxWeight, 2) /10, mass: 1, type:'curve'}));
             });
-            console.log(contactID);
-            console.log(relatedNodes);
+
             //compute node barycentre
-            let xSum = 0;
-            let ySum = 0;
-            _.forEach(relatedNodes, (n) => {
-                if( !n ) return;
-                xSum += n.x;
-                ySum += n.y;
-            });
-            xSum /= relatedNodes.length;
-            ySum /= relatedNodes.length;
-            contactNode.setPosition(xSum,ySum);
+
+            xSum /= total;
+            ySum /= total;
+            if( Number.isNaN(xSum) || Number.isNaN(ySum))
+            {
+                console.log(contactID);
+            }
+            else {
+                contactNode.setPosition(xSum,ySum);
+            }
+
         });
 
-        /*let skelTimestamps = [];
 
-        //first skeletton node
-        let firstNode = new Node(currentDate.valueOf(), {color: "#209ebe", label: currentDate.format( timeFormats[ granularity.type ] ), labelColor: "node", size: 2, mass: 1e-10, type: "skeleton"}) ;
-        skelTimestamps.push(firstNode.id);
-        let prevNode = firstNode;
-
-        //create skelleton nodes
-        while(currentDate <= endDate){
-            currentDate.add(granularity.increment, granularity.type);
-            let target = new Node(currentDate.valueOf(), {color: "#209ebe", label:currentDate.format( timeFormats[ granularity.type ] ), labelColor: "node", size: 0.000001, mass: 1e-10, type: "skeleton"});
-            res.addEdge(new Edge(prevNode, target, {weight: skeletonStrength, color: "#209ebe", size: 10}));
-            skelTimestamps.push(target.id);
-            prevNode = target;
-        }
-        res.addEdge(new Edge(prevNode, firstNode, {weight: skeletonStrength, color: "#209ebe", size: 10}));
-
-
-        /*let maxContactWeight = _.max(_.map(parsedData.contacts, msgs => _.sumBy(msgs, "weight")));
-        //link the contacts to the skeleton according to the messages
-        _.forEach(parsedData.contacts,( messages, contactID ) => {
-            let contactNode = new Node(contactID, {color: "#d54209", labelColor: "node", size: Math.sqrt((_.sumBy(messages, "weight")) / maxContactWeight) * 33 + 2});
-            let maxWeight = _.maxBy(messages, "weight").weight;
-            _.forEach(messages, (msg) => {
-
-                let msgTimestamp = msg.timestamp;
-
-                let index = interpolationSearch(skelTimestamps, msgTimestamp);
-
-                let target = new Node(skelTimestamps[index]);
-
-                res.addEdge(new Edge(contactNode, target, {color: "rgba(0,0,0,0.05)", weight: Math.pow(msg.weight / maxWeight, 2) /10, mass: 1, type:'curve'}));
-            });
-        });*/
         return res;
     }
 }
