1dcaecd0a467036c09c7101265ad5f6da5f821ce
KerDelos <damien.delmas@etu.utc.fr>
2017-01-19 14:09:18 +0100

    quick and dirty fix for circular graph of a day ( currently only works with sms input )

--- src/helpers/skelettonBuilders/CircularSkelettonBuilder.js
+++ src/helpers/skelettonBuilders/CircularSkelettonBuilder.js
@@ -12,8 +12,8 @@ const Node = require("../../classes/Node");
 const Edge = require("../../classes/Edge");
 
 class CircularSkelettonBuilder{
-    static build(parsedData, granularity){
 
+    static yearPOC(parsedData){
         const skeletonStrength = 0.5;
 
         let res = new Graph();
@@ -23,7 +23,7 @@ class CircularSkelettonBuilder{
         let endDate = moment(currentDate).add(365, 'days');
 
         let timeFormats = {
-            days:"DD-MM-YYYY",
+            days:"D MMMM",
             months:"MM-YYYY",
             years:"YYYY",
         };
@@ -31,7 +31,7 @@ class CircularSkelettonBuilder{
         //draw circleshape
         //(x0,y0) and whose radius is r is (x0 + r cos theta, y0 + r sin theta). Now choose theta values evenly spaced between 0 and 2pi.
         let incr = 6.29/365;
-        let theta = 0;
+        let theta = 6.29*1.5;
         let r = 25;
 
         let skelTimestamps = [];
@@ -70,9 +70,210 @@ class CircularSkelettonBuilder{
                 let msgTimestamp = moment(msg.timestamp).year(1970).valueOf();
                 let index = interpolationSearch(skelTimestamps, msgTimestamp);
                 if(index === -1) {
-                    console.log(moment(0).dayOfYear(moment(msg.timestamp).dayOfYear()).format("DD-MM-YYYY"));
+                    //console.log(moment(0).dayOfYear(moment(msg.timestamp).dayOfYear()).format("DD-MM-YYYY"));
                     var target = new Node(skelTimestamps[0]);
-                    return;
+                    //var target = new Node(moment(0).dayOfYear(moment(msg.timestamp).dayOfYear()));
+                }
+                else {
+                    var target = new Node(skelTimestamps[index]);
+                }
+                if( skelTimestamps[index] === undefined ) {
+                    //console.log( msgTimestamp +" ----->"+moment(msg.timestamp).format( "DD-MM-YYYY"));
+                }
+
+                let n = skelNodes[index];
+                if( n ){
+                    xSum += n.x*msg.weight;
+                    ySum += n.y*msg.weight;
+                    total += msg.weight;
+                }
+
+
+                res.addEdge(new Edge(contactNode, target, {color: "rgba(0,0,0,0.05)", weight: Math.pow(msg.weight / maxWeight, 2) /10, mass: 1, type:'curve'}));
+            });
+
+            //compute node barycentre
+
+            xSum /= total;
+            ySum /= total;
+            if( Number.isNaN(xSum) || Number.isNaN(ySum))
+            {
+                //console.log(contactID);
+            }
+            else {
+                contactNode.setPosition(xSum,ySum);
+            }
+
+        });
+
+
+        return res;
+    }
+
+    //TODO REFACTOR NEEDED, THIS IS JUST A QUICK HACK FOR THE POC
+    static dayPOC(parsedData){
+
+        const skeletonStrength = 0.5;
+
+        let res = new Graph();
+        res.metadata.type = Graph.TYPE.CircularSkelettonGraph;
+
+        let currentDate = moment(0);
+        let endDate = moment(currentDate).add(1, 'days').subtract(10,'minutes');
+
+        let timeFormats = {
+            days:"HH:mm",
+            months:"MM-YYYY",
+            years:"YYYY",
+        };
+
+        //draw circleshape
+        //(x0,y0) and whose radius is r is (x0 + r cos theta, y0 + r sin theta). Now choose theta values evenly spaced between 0 and 2pi.
+        let incr = 6.29/(24*6);
+        let theta = 6.29*0.75;
+        let r = 25;
+
+        let skelTimestamps = [];
+        let skelNodes = [];
+
+
+        //first skeletton node
+        let firstNode = new Node(currentDate.valueOf(), {x: r* Math.cos(theta), y: r * Math.sin(theta), color: "#209ebe", label: currentDate.format( timeFormats[ 'days' ] ), labelColor: "node", size: 2, mass: 1e-10, type: "skeleton"}) ;
+        let prevNode = firstNode;
+        skelTimestamps.push(firstNode.id);
+        skelNodes.push(firstNode);
+
+        //create skelleton nodes
+        while(currentDate < endDate){
+            theta += incr;
+            currentDate.add(10, 'minutes');
+            let target = new Node(currentDate.valueOf(), {x: r*Math.cos(theta), y: r * Math.sin(theta), fixed: false, color: "#209ebe", label:currentDate.format( timeFormats[ 'days' ] ), labelColor: "node", size: 0.000001, mass: 1e-10, type: "skeleton"});
+            res.addEdge(new Edge(prevNode, target, {weight: skeletonStrength, color: "#209ebe", size: 10}));
+            skelTimestamps.push(target.id);
+            skelNodes.push(target);
+            prevNode = target;
+        }
+        res.addEdge(new Edge(prevNode, firstNode, {weight: skeletonStrength, color: "#209ebe", size: 10}));
+
+        let maxContactWeight = _.max(_.map(parsedData.contacts, msgs => _.sumBy(msgs, "weight")));
+        //link the contacts to the skeleton according to the messages
+        _.forEach(parsedData.contacts,( messages, contactID ) => {
+            let contactNode = new Node(contactID, {color: "#d54209", labelColor: "node", size: Math.sqrt((_.sumBy(messages, "weight")) / maxContactWeight) * 33 + 2});
+            let maxWeight = _.maxBy(messages, "weight").weight;
+
+            let xSum = 0;
+            let ySum = 0;
+            let total = 0;
+            _.forEach(messages, (msg) => {
+
+                let msgTimestamp = moment(msg.timestamp).year(1970).dayOfYear(1).valueOf();
+                let index = interpolationSearch(skelTimestamps, msgTimestamp);
+                if(index === -1) {
+                    //console.log(moment(0).dayOfYear(moment(msg.timestamp).dayOfYear()).format("DD-MM-YYYY"));
+                    var target = new Node(skelTimestamps[0]);
+                    //var target = new Node(moment(0).dayOfYear(moment(msg.timestamp).dayOfYear()));
+                }
+                else {
+                    var target = new Node(skelTimestamps[index]);
+                }
+                if( skelTimestamps[index] === undefined ) {
+                    //console.log( msgTimestamp +" ----->"+moment(msg.timestamp).format( "DD-MM-YYYY"));
+                }
+
+                let n = skelNodes[index];
+                if( n ){
+                   //console.log("ok");
+                    xSum += n.x*msg.weight;
+                    ySum += n.y*msg.weight;
+                    total += msg.weight;
+                }
+                else {
+                    //console.log("wrong");
+                }
+
+
+                res.addEdge(new Edge(contactNode, target, {color: "rgba(0,0,0,0.05)", weight: Math.pow(msg.weight / maxWeight, 2) /10, mass: 1, type:'curve'}));
+            });
+
+            //compute node barycentre
+
+            xSum /= total;
+            ySum /= total;
+            if( Number.isNaN(xSum) || Number.isNaN(ySum))
+            {
+                //console.log(contactID);
+            }
+            else {
+                contactNode.setPosition(xSum,ySum);
+            }
+
+        });
+
+
+        return res;
+    }
+
+    //TODO REFACTOR NEEDED, THIS IS JUST A QUICK HACK FOR THE POC
+    static weekPOC(parsedData){
+
+        const skeletonStrength = 0.5;
+
+        let res = new Graph();
+        res.metadata.type = Graph.TYPE.CircularSkelettonGraph;
+
+        let currentDate = moment(0);
+        let endDate = moment(currentDate).add(7, 'days').subtract(1,'hours');
+
+        let timeFormats = {
+            days:"ddd HH:mm",
+            months:"MM-YYYY",
+            years:"YYYY",
+        };
+
+        //draw circleshape
+        //(x0,y0) and whose radius is r is (x0 + r cos theta, y0 + r sin theta). Now choose theta values evenly spaced between 0 and 2pi.
+        let incr = 6.29/(24*6);
+        let theta = 6.29*0.75;
+        let r = 25;
+
+        let skelTimestamps = [];
+        let skelNodes = [];
+
+
+        //first skeletton node
+        let firstNode = new Node(currentDate.valueOf(), {x: r* Math.cos(theta), y: r * Math.sin(theta), color: "#209ebe", label: currentDate.format( timeFormats[ 'days' ] ), labelColor: "node", size: 2, mass: 1e-10, type: "skeleton"}) ;
+        let prevNode = firstNode;
+        skelTimestamps.push(firstNode.id);
+        skelNodes.push(firstNode);
+
+        //create skelleton nodes
+        while(currentDate < endDate){
+            theta += incr;
+            currentDate.add(1, 'hours');
+            let target = new Node(currentDate.valueOf(), {x: r*Math.cos(theta), y: r * Math.sin(theta), fixed: false, color: "#209ebe", label:currentDate.format( timeFormats[ 'days' ] ), labelColor: "node", size: 0.000001, mass: 1e-10, type: "skeleton"});
+            res.addEdge(new Edge(prevNode, target, {weight: skeletonStrength, color: "#209ebe", size: 10}));
+            skelTimestamps.push(target.id);
+            skelNodes.push(target);
+            prevNode = target;
+        }
+        res.addEdge(new Edge(prevNode, firstNode, {weight: skeletonStrength, color: "#209ebe", size: 10}));
+
+        let maxContactWeight = _.max(_.map(parsedData.contacts, msgs => _.sumBy(msgs, "weight")));
+        //link the contacts to the skeleton according to the messages
+        _.forEach(parsedData.contacts,( messages, contactID ) => {
+            let contactNode = new Node(contactID, {color: "#d54209", labelColor: "node", size: Math.sqrt((_.sumBy(messages, "weight")) / maxContactWeight) * 33 + 2});
+            let maxWeight = _.maxBy(messages, "weight").weight;
+
+            let xSum = 0;
+            let ySum = 0;
+            let total = 0;
+            _.forEach(messages, (msg) => {
+
+                let msgTimestamp = moment(msg.timestamp).year(1970).day(1).valueOf();
+                let index = interpolationSearch(skelTimestamps, msgTimestamp);
+                if(index === -1) {
+                    //console.log(moment(0).dayOfYear(moment(msg.timestamp).dayOfYear()).format("DD-MM-YYYY"));
+                    var target = new Node(skelTimestamps[0]);
                     //var target = new Node(moment(0).dayOfYear(moment(msg.timestamp).dayOfYear()));
                 }
                 else {
@@ -84,9 +285,13 @@ class CircularSkelettonBuilder{
 
                 let n = skelNodes[index];
                 if( n ){
+                    console.log("ok");
                     xSum += n.x*msg.weight;
                     ySum += n.y*msg.weight;
                     total += msg.weight;
+                }
+                else {
+                    console.log("wrong");
                 }
 
 
@@ -109,6 +314,22 @@ class CircularSkelettonBuilder{
 
 
         return res;
+    }
+
+
+
+    static build(parsedData, granularity) {
+        console.log(granularity);
+
+        if (granularity.type === "days") {
+            return this.dayPOC(parsedData);
+        }
+        else if (granularity.type === "years") {
+            return this.yearPOC(parsedData);
+        }
+        else if( granularity.type ==="months"){
+            return this.weekPOC(parsedData);
+        }
     }
 }
 
